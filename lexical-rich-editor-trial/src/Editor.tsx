9e6bc0ba283fd45eba065bf225ebe4d4055fc683
raiga0310 <raiga0310brilio@gmail.com>
2022-11-02 14:56:34 +0900

    init

---
+++ lexical-rich-editor-trial/src/Editor.tsx
@@ -0,0 +1,43 @@
+import { ComponentProps, FC } from "react";
+import { CheckListPlugin } from "@lexical/react/LexicalCheckListPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { ListPlugin } from "@lexical/react/LexicalListPlugin";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import styles from "./Editor.module.scss";
+import { theme } from "./editorTheme";
+import { nodes } from "./nodes";
+import { AutoFocusPlugin } from "./plugins/AutoFocusPlugin";
+import { CodeHighlightPlugin } from "./plugins/CodeHighlightPlugin";
+import { InlineToolbarPlugin } from "./plugins/InlineToolbarPlugin";
+import { ToolbarPlugin } from "./plugins/ToolbarPlugin";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
+const initialConfig: ComponentProps<typeof LexicalComposer>["initialConfig"] = {
+    namespace: "MyEditor",
+    onError: error => console.error(error),
+    nodes: nodes,
+    theme: theme,
+};
+
+export const Editor: FC = () => {
+    return (
+        <LexicalComposer initialConfig={initialConfig}>
+            <ToolbarPlugin />
+            <InlineToolbarPlugin />
+            <div className={styles.editorContainer}>
+                <RichTextPlugin
+                    contentEditable={<ContentEditable className={styles.contentEditable} />}
+                    placeholder={<div className={styles.placeholder}>いまなにしてる？</div>}
+                    ErrorBoundary={LexicalErrorBoundary}
+                />
+            </div>
+            <AutoFocusPlugin />
+            <HistoryPlugin />
+            <ListPlugin />
+            <CheckListPlugin />
+            <CodeHighlightPlugin />
+        </LexicalComposer>
+    );
+};
